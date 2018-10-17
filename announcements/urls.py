3dced65190132e0a428058e3fb8323a6d68c01ea
Sven Redsun <svengato@svengato.com>
2018-10-17 19:43:09 -0400

    Add app_name, title, nested templates, and otherwise clean up Announcements template files and urls.py

--- announcements/urls.py
+++ announcements/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "announcements"
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
     url(r'^past/$', views.past, name = 'past'),
